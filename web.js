bbbffa8e3e8ec52813496e9e98b39ddf89fba3d8
Michael BRIGHT <mjbrightfr+git-kernel@gmail.com>
2013-07-08 22:57:47 +0200

    Modified web.js to read index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  filec = fs.readFile('index.html');
+  response.send(filec.toString('utf8'));
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
