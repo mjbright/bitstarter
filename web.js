e15ae39147d6489336bf6a7df3168e123b8b4277
Michael BRIGHT <mjbrightfr+git-kernel@gmail.com>
2013-07-21 23:42:08 +0200

    HW4 part1

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var fs = require('fs');
 
@@ -13,7 +14,7 @@ app.get('/', function(request, response) {
   //response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
